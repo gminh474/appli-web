e000517de614736123e578452c82401cfb0202e5
Jim Etevenard <jetevenard@oxiane.com>
2022-02-16 20:36:08 +0100

    Clarification des tests

--- test/test.js
+++ test/test.js
@@ -2,25 +2,30 @@ var assert = require('assert');
 
 const prefix = require('../js/prefixer');
 
-console.log(prefix('jim', 'Je suis une chaine'));
-
 describe('Prefixer tests', function() {
 
   describe('Simple case', function() {
     it('Input is prefixed', function() {
-      assert.equal( prefix('YO', 'A string to prefix'), '[YO] A string to prefix');
+
+      // Ceci est le résultat attendu :
+      const expectedResult = '[YO] A string to prefix';
+
+      // Nous testons que la fonction prefix retourne bien le résultat attendu
+      assert.equal( prefix('YO', 'A string to prefix'), expectedResult);
     });
   });
 
   describe('LowerCase input', function() {
     it('Should uppercase the prefix', function() {
-      assert.equal( prefix('JIM', 'A string to prefix'), '[JIM] A string to prefix');
+      const expectedResult = '[JIM] A string to prefix';
+      assert.equal( prefix('jim', 'A string to prefix'), expectedResult);
     });
   });
 
   describe('Trimed String', function() {
     it('The string to prefix should be trimed.', function() {
-      assert.equal( prefix('JIM', '    A lot of space around !    '), '[JIM] A lot of space around !');
+      const expectedResult = '[JIM] A lot of space around !'
+      assert.equal( prefix('JIM', '    A lot of space around !    '), expectedResult);
     });
   });
 
